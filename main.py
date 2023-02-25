0f766fd81b7d7198cf038df023605c4a9b26d4fb
dRamilowski <67924799+beyus723@users.noreply.github.com>
2023-02-25 21:11:55 +0000

    creating a draft for the project

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
- 
+print("-" *7, " GRADE GENERATOR", "-" *7, "\n")
+print("# Python Skills Test\n")
+
+#ask the users what is maximum score and how many points they received
+
+maxScore = int(input("What is the maximum score of the test?\n"))
+
+pointsReceived = int(input("How many points did you receive?\n"))
+
+
+
+#Figure out the percentage the user received and round to 2 decimal places.
+
+
+
+
+
+#Use if/elif statements to show users the letter grade they received.
+
+
+
+
+
+#At the end, the user should see the letter grade they received and the percentage correct.
