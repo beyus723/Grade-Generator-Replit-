a7687fbc14049e274b9a421560b5c6ead7d78e6e
dRamilowski <67924799+beyus723@users.noreply.github.com>
2023-02-25 21:45:33 +0000

    score percentage calculator

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
+
 print("-" *7, " GRADE GENERATOR", "-" *7, "\n")
 print("# Python Skills Test\n")
 
 #ask the users what is maximum score and how many points they received
 
-maxScore = int(input("What is the maximum score of the test?\n"))
+maxScore = float(input("What is the maximum score of the test?\n\n"))
 
-pointsReceived = int(input("How many points did you receive?\n"))
-
-
+pointsReceived = float(input("How many points did you receive?\n\n"))
 
 #Figure out the percentage the user received and round to 2 decimal places.
 
-
-
+percentage = (pointsReceived * 100) / maxScore
+percentage = round(percentage, 2)
+#print(percentage, "%")
 
 
 #Use if/elif statements to show users the letter grade they received.
 
+print("")
+print("-" * 7, "Your score", "-" * 7)
+print("\nYou received ", int(pointsReceived), " points of ", int(maxScore), " maximum points, which is ", percentage, "% of the total test score.")
 
 
 
